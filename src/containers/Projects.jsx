65a0ec7fcd483195f4a3679080ed5f29e63ee81d
KrlozMedina <camedinal@correo.udistrital.edu.co>
2022-08-28 17:19:45 -0500

    [ADD] Proyectos

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -49,6 +49,73 @@ const Projects = (children) => {
               <div className="code-logo" /> Code
             </a>
           </article>
+
+          <article className="project-card">
+            <a href="https://rickandmorty-krlozmedina.netlify.app/">
+              <div className="project-card--image rick-morty" />
+              <h3 className="project-card--title">Rick&Morty</h3>
+              <p className="project-card--description">Reack Hooks con la API de Rick and Morty</p>
+            </a>
+            <a className="project--code-link" href="https://github.com/KrlozMedina/Rick-Morty">
+              <div className="code-logo" /> Code
+            </a>
+          </article>
+
+          <article className="project-card">
+            <a href="https://krlozmedina.github.io/Exchange/">
+              <div className="project-card--image exchange" />
+              <h3 className="project-card--title">Exchange</h3>
+              <p className="project-card--description">Usando API de criptomonedas, observando graficas con chart.js. Tiene falla de CORS</p>
+            </a>
+            <a className="project--code-link" href="https://github.com/KrlozMedina/Python">
+              <div className="code-logo" /> Code
+            </a>
+          </article>
+
+          <article className="project-card">
+            <a href="https://github.com/KrlozMedina/Python">
+              <div className="project-card--image" />
+              <h3 className="project-card--title">Python</h3>
+              <p className="project-card--description">Proyectos en el lenguaje Python</p>
+            </a>
+            <a className="project--code-link" href="https://github.com/KrlozMedina/Python">
+              <div className="code-logo" /> Code
+            </a>
+          </article>
+
+          <article className="project-card">
+            <a href="https://github.com/KrlozMedina/Matlab">
+              <div className="project-card--image" />
+              <h3 className="project-card--title">Matlab</h3>
+              <p className="project-card--description">Proyectos en el lenguaje Matlab</p>
+            </a>
+            <a className="project--code-link" href="https://github.com/KrlozMedina/Matlab">
+              <div className="code-logo" /> Code
+            </a>
+          </article>
+
+          <article className="project-card">
+            <a href="https://github.com/KrlozMedina/Arduino">
+              <div className="project-card--image" />
+              <h3 className="project-card--title">Arduino</h3>
+              <p className="project-card--description">Proyectos en el lenguaje Arduino</p>
+            </a>
+            <a className="project--code-link" href="https://github.com/KrlozMedina/Arduino">
+              <div className="code-logo" /> Code
+            </a>
+          </article>
+
+          <article className="project-card">
+            <a href="https://github.com/KrlozMedina/Java">
+              <div className="project-card--image" />
+              <h3 className="project-card--title">Java</h3>
+              <p className="project-card--description">Proyectos en el lenguaje Java</p>
+            </a>
+            <a className="project--code-link" href="https://github.com/KrlozMedina/Java">
+              <div className="code-logo" /> Code
+            </a>
+          </article>
+
         </div>
       </section>
   )
