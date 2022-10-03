cf2daa21f185716a60b35cae1ebd86462ec0008c
KrlozMedina <charli1216@hotmail.com>
2022-10-02 20:16:13 -0500

    [REACT] Codigo reducido en projects

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,121 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '@styles/Projects.scss'
 
-const Projects = (children) => {
+const projectsData = require('../storage/projects.json');
+
+const Projects = () => {
   return (
      <section id='projects' className="main-projects-container">
         <h2>PROYECTOS</h2>
         <div className="projects-cards-container">
-          <article className="project-card">
-            <a href="https://krlozmedina.github.io/Cronometro/">
-              <div className="project-card--image cronometro" />
-              <h3 className="project-card--title">Cronometro</h3>
-              <p className="project-card--description">Cronometro y temporizador en una sola aplicacion web</p>
-            </a>
-            <a className="project--code-link" href="https://github.com/KrlozMedina/Cronometro">
-              <div className="code-logo" /> Code
-            </a>
-          </article>
-
-          <article className="project-card">
-            <a href="https://krlozmedina.github.io/BatataBit/">
-              <div className="project-card--image batatabit" />
-              <h3 className="project-card--title">Clon Batata Bit</h3>
-              <p className="project-card--description">Clone de la pagina Batata Bit</p>
-            </a>
-            <a className="project--code-link" href="https://github.com/KrlozMedina/BatataBit">
-              <div className="code-logo" /> Code
-            </a>
-          </article>
-          
-          <article className="project-card">
-            <a href="https://krlozmedina.github.io/RandomDogs/">
-              <div className="project-card--image randomdogs" />
-              <h3 className="project-card--title">Random Dogs</h3>
-              <p className="project-card--description">Consumo de API de perros</p>
-            </a>
-            <a className="project--code-link" href="https://github.com/KrlozMedina/RandomDogs">
-              <div className="code-logo" /> Code
-            </a>
-          </article>
-
-          <article className="project-card">
-            <a href="https://mypokedesk.herokuapp.com/">
-              <div className="project-card--image pokemon" />
-              <h3 className="project-card--title">Pokemon</h3>
-              <p className="project-card--description">Minijuego usando la API de Pokemon</p>
-            </a>
-            <a className="project--code-link" href="https://github.com/KrlozMedina/MyPokedesk">
-              <div className="code-logo" /> Code
-            </a>
-          </article>
-
-          <article className="project-card">
-            <a href="https://rickandmorty-krlozmedina.netlify.app/">
-              <div className="project-card--image rick-morty" />
-              <h3 className="project-card--title">Rick&Morty</h3>
-              <p className="project-card--description">Reack Hooks con la API de Rick and Morty</p>
-            </a>
-            <a className="project--code-link" href="https://github.com/KrlozMedina/Rick-Morty">
-              <div className="code-logo" /> Code
-            </a>
-          </article>
-
-          <article className="project-card">
-            <a href="https://krlozmedina.github.io/Exchange/">
-              <div className="project-card--image exchange" />
-              <h3 className="project-card--title">Exchange</h3>
-              <p className="project-card--description">Usando API de criptomonedas, observando graficas con chart.js. Tiene falla de CORS</p>
-            </a>
-            <a className="project--code-link" href="https://github.com/KrlozMedina/Python">
-              <div className="code-logo" /> Code
-            </a>
-          </article>
-
-          <article className="project-card">
-            <a href="https://github.com/KrlozMedina/Python">
-              <div className="project-card--image" />
-              <h3 className="project-card--title">Python</h3>
-              <p className="project-card--description">Proyectos en el lenguaje Python</p>
-            </a>
-            <a className="project--code-link" href="https://github.com/KrlozMedina/Python">
-              <div className="code-logo" /> Code
-            </a>
-          </article>
-
-          <article className="project-card">
-            <a href="https://github.com/KrlozMedina/Matlab">
-              <div className="project-card--image" />
-              <h3 className="project-card--title">Matlab</h3>
-              <p className="project-card--description">Proyectos en el lenguaje Matlab</p>
-            </a>
-            <a className="project--code-link" href="https://github.com/KrlozMedina/Matlab">
-              <div className="code-logo" /> Code
-            </a>
-          </article>
-
-          <article className="project-card">
-            <a href="https://github.com/KrlozMedina/Arduino">
-              <div className="project-card--image" />
-              <h3 className="project-card--title">Arduino</h3>
-              <p className="project-card--description">Proyectos en el lenguaje Arduino</p>
-            </a>
-            <a className="project--code-link" href="https://github.com/KrlozMedina/Arduino">
-              <div className="code-logo" /> Code
-            </a>
-          </article>
-
-          <article className="project-card">
-            <a href="https://github.com/KrlozMedina/Java">
-              <div className="project-card--image" />
-              <h3 className="project-card--title">Java</h3>
-              <p className="project-card--description">Proyectos en el lenguaje Java</p>
-            </a>
-            <a className="project--code-link" href="https://github.com/KrlozMedina/Java">
-              <div className="code-logo" /> Code
-            </a>
-          </article>
-
+          {
+            projectsData.map(project => 
+              <article className="project-card">
+                <a href={project['url-project']}>
+                  <div className={`project-card--image ${project.image}`} />
+                  <h3 className="project-card--title">{project.title}</h3>
+                  <p className="project-card--description">{project.description}</p>
+                </a>
+                <a className="project--code-link" href={project['url-project-code']}>
+                  <div className="code-logo" /> Code
+                </a>
+              </article>
+            )
+          }
         </div>
       </section>
   )
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
       '@styles': path.resolve(__dirname, '/src/styles'),
       '@images': path.resolve(__dirname, '/src/assets/images'),
       '@icons': path.resolve(__dirname, './src/assets/icons'),
+      '@storage': path.resolve(__dirname, './src/storage'),
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@containers': path.resolve(__dirname, './src/containers'),
